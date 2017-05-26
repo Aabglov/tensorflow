9fa9de86d941b80c7f9a89895701c5f5db92cc6d
Kegan Rabil <keganrabil@galaxy-s6-edge.gcs-research.net>
2017-05-26 16:17:47 -0600

    Fixed tensorboard issue

--- conv_net.py
+++ conv_net.py
@@ -1,10 +1,7 @@
-import argparse
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 import pickle
 import os
-from datetime import datetime
 import time
 
 # PATHS
@@ -15,19 +12,14 @@ DATA_PATH = os.path.join(DIR_PATH,"data","notMNIST.pkl")
 CSV_PATH = os.path.join(DIR_PATH,"test.csv")
 
 
-# with open(CSV_PATH,"w+") as f:
-#     for i in range(len(train)):
-#         f.write(",".join(["{:f}".format(t) for t in train[i].ravel()]) +
-#                 "," +
-#                 "{:f}".format(labels[i]) +
-#                 "\n")
-#
-# HODOR
-
 # Set random seed
 seed = 36 # Pick your favorite
 np.random.seed(seed)
 tf.set_random_seed(seed)
+
+if tf.gfile.Exists(LOG_DIR):
+    tf.gfile.DeleteRecursively(LOG_DIR)
+tf.gfile.MakeDirs(LOG_DIR)
 
 ######################################### CONSTANTS ########################################
 NUM_CLASSES = 10 # The number of digits present in the dataset
@@ -41,7 +33,7 @@ HIDDEN_SIZE_2 = 256 # 2nd layer number of features
 INPUT_SIZE = 784 # MNIST data input (img shape: 28*28)
 NUM_CLASSES = 10 # MNIST total classes (0-9 digits)
 
-BATCH_SIZE = 128
+BATCH_SIZE = 1024
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 MAX_STEPS = 10000
@@ -106,7 +98,7 @@ with graph.as_default():
         image_shaped_input = tf.reshape(x,[-1,IMG_SIZE, IMG_SIZE, 1])
         tf.summary.image('input', image_shaped_input, NUM_CLASSES)
         shaped_input = tf.reshape(x, [-1, INPUT_SIZE])
-        shaped_labels = tf.one_hot(y,NUM_CLASSES)
+        shaped_labels = tf.reshape(tf.one_hot(y,NUM_CLASSES),[-1,NUM_CLASSES])
 
     # We can't initialize these variables to 0 - the network will get stuck.
     def init_weight(shape):
@@ -175,67 +167,67 @@ with graph.as_default():
     saver = tf.train.Saver()
 
 #Running first session
-def main():
-    with tf.Session(graph=graph) as sess:
-        # Initialize variables
-        sess.run(init)
+#def main():
+with tf.Session(graph=graph) as sess:
+    # Initialize variables
+    sess.run(init)
 
-        try:
-            saver.restore(sess, SAVE_PATH)
-            print("Model restored from file: %s" % SAVE_PATH)
-        except Exception as e:
-            print("Model restore failed {}".format(e))
+    try:
+        saver.restore(sess, SAVE_PATH)
+        print("Model restored from file: %s" % SAVE_PATH)
+    except Exception as e:
+        print("Model restore failed {}".format(e))
 
-        train_batcher = Batcher(TRAIN_DATASET,TRAIN_LABELS)
-        #test_batcher =  Batcher(TEST_DATASET, TEST_DATASET)
-        total_batch = int(len(TRAIN_DATASET)/BATCH_SIZE)
+    train_batcher = Batcher(TRAIN_DATASET,TRAIN_LABELS)
+    #test_batcher =  Batcher(TEST_DATASET, TEST_DATASET)
+    total_batch = int(len(TRAIN_DATASET)/BATCH_SIZE)
 
-        # Training cycle
-        for epoch in range(MAX_STEPS):
-            avg_cost = 0.
+    # Training cycle
+    for epoch in range(MAX_STEPS):
+        avg_cost = 0.
 
-            # Loop over all batches
-            for i in range(total_batch):
-                batch_x, batch_y = train_batcher.nextBatch(BATCH_SIZE)
-                # Run optimization op (backprop) and cost op (to get loss value)
-                summary, _ = sess.run([merged, train_step], feed_dict={x: batch_x, y: batch_y})
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = train_batcher.nextBatch(BATCH_SIZE)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            summary, _ = sess.run([merged, train_step], feed_dict={x: batch_x, y: batch_y})
+            train_writer.add_summary(summary, i)
+
+            # Keep track of meta data
+            if i % 100 == 0:
+                #   I'm not a billion percent sure what this does....
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                summary, _ = sess.run([merged, train_step],
+                                      feed_dict={x: batch_x, y: batch_y},
+                                      options=run_options,
+                                      run_metadata=run_metadata)
+                train_writer.add_run_metadata(run_metadata, "step_{}_{}".format(epoch,i))
                 train_writer.add_summary(summary, i)
+                print('Adding run metadata for', i)
 
-                # Keep track of meta data
-                if i % 100 == 0:
-                    #   I'm not a billion percent sure what this does....
-                    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-                    run_metadata = tf.RunMetadata()
-                    summary, _ = sess.run([merged, train_step],
-                                          feed_dict={x: batch_x, y: batch_y},
-                                          options=run_options,
-                                          run_metadata=run_metadata)
-                    train_writer.add_run_metadata(run_metadata, "step_{}_{}".format(epoch,i))
-                    train_writer.add_summary(summary, i)
-                    print('Adding run metadata for', i)
+        # Display logs per epoch step
+        if epoch % LOG_FREQUENCY == 0:
+            summary, acc = sess.run([merged, accuracy], feed_dict={x: TEST_DATASET, y: TEST_LABELS})
+            test_writer.add_summary(summary, i)
+            print('Accuracy at step %s: %s' % (epoch, acc))
+    # Cleanup
+    train_writer.close()
+    test_writer.close()
+    print("Training Finished!")
 
-            # Display logs per epoch step
-            if epoch % LOG_FREQUENCY == 0:
-                summary, acc = sess.run([merged, accuracy], feed_dict={x: TEST_DATASET, y: TEST_LABELS})
-                test_writer.add_summary(summary, i)
-                print('Accuracy at step %s: %s' % (epoch, acc))
-        # Cleanup
-        train_writer.close()
-        test_writer.close()
-        print("Training Finished!")
+    # Test model
+    _,acc = sess.run([correct_prediction,accuracy], feed_dict={x: TEST_DATASET, y: TEST_LABELS})
+    print("Accuracy:", acc)
 
-        # Test model
-        _,acc = sess.run([correct_prediction,accuracy], feed_dict={x: TEST_DATASET, y: TEST_LABELS})
-        print("Accuracy:", acc)
-
-        # Save model weights to disk
-        save_path = saver.save(sess, SAVE_PATH)
-        print("Model saved in file: %s" % save_path)
+    # Save model weights to disk
+    save_path = saver.save(sess, SAVE_PATH)
+    print("Model saved in file: %s" % save_path)
 
 
-if __name__ == "__main__":
-    # Cleanup previous training log
-    if tf.gfile.Exists(LOG_DIR):
-        tf.gfile.DeleteRecursively(LOG_DIR)
-    tf.gfile.MakeDirs(LOG_DIR)
-    main()
+# if __name__ == "__main__":
+#     # Cleanup previous training log
+#     if tf.gfile.Exists(LOG_DIR):
+#         tf.gfile.DeleteRecursively(LOG_DIR)
+#     tf.gfile.MakeDirs(LOG_DIR)
+#     main()
