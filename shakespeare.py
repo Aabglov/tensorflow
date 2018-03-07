55dbc0fdf90c0cc363b905874cac58c320807e89
Kegan Rabil <keganrabil@Kegans-Mac-mini.gcs-research.net>
2018-03-06 17:12:52 -0700

    added mask seq - this slowed everything down a lot

--- shakespeare.py
+++ shakespeare.py
@@ -67,7 +67,7 @@ args = {
     'n_classes':WH.vocab.vocab_size,
     'lstm_size':512,
     'num_layers':3, #2
-    'num_steps':1 #250
+    'num_steps':50 #250
 }
 
 
@@ -96,6 +96,7 @@ with tf.device('/cpu:0'):
         # Placeholders
         x = tf.placeholder(tf.int32, [None, 1], name='input_placeholder')
         y = tf.placeholder(tf.int32, [None, 1], name='labels_placeholder')
+        mask_seq = tf.placeholder(tf.float32, [None, 1], name='mask_sequence_placeholder')
         dropout_prob = tf.placeholder(tf.float32)
 
         # Our initial state placeholder:
@@ -147,8 +148,8 @@ with tf.device('/cpu:0'):
         #pred = tf.nn.softmax(logits)
         pred = tf.nn.softmax(tf.div(logits,temp))
 
-        losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.reshape(y,[-1,]), logits=tf.reshape(logits,[-1,N_CLASSES]))
-        cost = tf.reduce_mean(losses)#tf.reduce_sum(losses)
+        losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+        cost = tf.div(tf.reduce_sum(losses * mask_seq), tf.cast(batch_size,tf.float32))
 
         lr = tf.Variable(0.0, trainable=False)
         tvars = tf.trainable_variables()
@@ -205,9 +206,15 @@ with tf.device('/cpu:0'):
                 for i in range(0,batch.shape[1]-1):
                     batch_x = batch[:,i].reshape((BATCH_SIZE,1))
                     batch_y = batch[:,(i+1)].reshape((BATCH_SIZE,1))
+                    #print(batch_y)
+                    mask = np.ones(batch_y.shape)
+                    for i in range(len(mask)):
+                        if y[i][0] == WH.TrainBatches.vocab.pad:
+                            mask[i][0] = 0.0
                     # Run optimization op (backprop) and cost op (to get loss value)
-                    _, s, c = sess.run([optimizer, final_state, cost], feed_dict={x: batch_x,
+                    _, s, c, l = sess.run([optimizer, final_state, cost, logits], feed_dict={x: batch_x,
                                                                                   y: batch_y,
+                                                                                  mask_seq: mask,
                                                                                   init_state: state,
                                                                                   dropout_prob: DROPOUT_KEEP_PROB,
                                                                                   temp:1.0})
