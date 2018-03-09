181d6dbf67eac52f94e8e4b49da4ef222ed4bd37
Kegan Rabil <keganrabil@Kegans-Mac-mini.gcs-research.net>
2018-03-09 12:33:38 -0700

    added sampler file, updated model, removed graph to accommodate

--- shakespeare.py
+++ shakespeare.py
@@ -23,55 +23,13 @@ checkpoint_path = os.path.join(dir_path,"saved",SAVE_DIR)
 data_path = os.path.join(dir_path,"data",SUBDIR_NAME,DATA_NAME)
 
 
-try:
-    with open(os.path.join(dir_path,"data",PICKLE_PATH),"rb") as f:
-        WH = pickle.load(f)
-except Exception as e:
-    print(e)
-    # What's with those weird symbols?
-    # u'\xbb' is our GO symbol (»)
-    # u'\xac' is our UNKNOWN symbol (¬)
-    # u'\xa4' is our END symbol (¤)
-    # They're arbitrarily chosen, but
-    # I think they both:
-    #   1). Are unlikely to appear in regular data, let alone cleaned data.
-    #   2). Look awesome.
-    vocab = [u'\xbb','|', '5', 'c', 'r', 'e', 'a', 't', 'u', '4', '6', 'h', 'm', 'n', ' ', 'o', 'd', 'l', 'i', '7', \
-             '8', '&', '^', '/', '9', '{', 'W', '}', ',', 'T', ':', 's', 'y', 'b', 'f', 'v', 'p', '.', '3', \
-             '0', 'A', '1', 'w', 'g', '\\', 'E', '@', '+', 'R', 'C', 'x', 'B', 'G', 'O', 'k', '"', 'N', 'U', \
-             "'", 'q', 'z', '-', 'Y', 'X', '*', '%', '[', '=', ']', '~', 'j', 'Q', 'L', 'S', 'P', '2',u'\xac',u'\xf8',u'\xa4',u'\u00BB']
-
-    # Load mtg tokenized data
-    # Special thanks to mtgencode: https://github.com/billzorn/mtgencode
-    with open(data_path,"r") as f:
-         # Each card occupies its own line in this tokenized version
-         raw_txt = f.read()#.split("\n")
-    WH = word_helpers.WordHelper(raw_txt, vocab)
-    #WH = word_helpers.JSONHelper(data_path,vocab)
-
-
-    # Save our WordHelper
-    with open(os.path.join(dir_path,"data",PICKLE_PATH),"wb") as f:
-        pickle.dump(WH,f)
-
-# max_len = 75 # Determined by going through entire corpus
-# batch = WH.TrainBatches.next_card_batch(100,1)
-# for b in batch:
-#     print("".join([WH.vocab.vocab[i] for i in b]))
-# HODOR
-
-def weighted_pick(weights):
-    t = np.cumsum(weights)
-    s = np.sum(weights)
-    return(int(np.searchsorted(t, np.random.rand(1)*s)))
-
 args = {
     'learning_rate':1e-3,#3e-4
     'grad_clip':5.0,
-    'n_input':WH.vocab.vocab_size,
-    'n_classes':WH.vocab.vocab_size,
-    'lstm_size':512, #128,
-    'num_layers':3, #2
+    'n_input': 105,#WH.vocab.vocab_size,
+    'n_classes': 105,#WH.vocab.vocab_size,
+    'lstm_size': 512, #128
+    'num_layers': 3, #2
     'num_steps':50 #250
 }
 
@@ -94,89 +52,137 @@ DROPOUT_KEEP_PROB = 1.0 #0.5
 TEMPERATURE = 1.0
 NUM_PRED = 50
 already_trained = 0
-PRIME_TEXT = "»To be or not to be"
+PRIME_TEXT = "»"
 
-with tf.device('/cpu:0'):
-    graph = tf.Graph()
-    with graph.as_default():
-        # Placeholders
-        x = tf.placeholder(tf.int32, [None, 1], name='input_placeholder')
-        y = tf.placeholder(tf.int32, [None, 1], name='labels_placeholder')
-        dropout_prob = tf.placeholder(tf.float32)
+def weighted_pick(weights):
+    t = np.cumsum(weights)
+    s = np.sum(weights)
+    return(int(np.searchsorted(t, np.random.rand(1)*s)))
 
-        # Our initial state placeholder:
-        # NUM_LAYERS -- the number of layers used by our stacked LSTM
-        # 2 -- 2 states for each layer (output,hidden)
-        # None -- This will be our batch_size which is flexible
-        # LSTM_SIZE -- the size of our hidden layers
-        init_state = tf.placeholder(tf.float32, [NUM_LAYERS, 2, None, LSTM_SIZE], name='state_placeholder')
-        # Create appropriate LSTMStateTuple for dynamic_rnn function out of our placeholder
-        l = tf.unstack(init_state, axis=0)
-        rnn_tuple_state = tuple(
-            [tf.contrib.rnn.LSTMStateTuple(l[idx][0], l[idx][1]) for idx in range(NUM_LAYERS)]
-        )
+def getWordHelpers():
+    try:
+        with open(os.path.join(dir_path,"data",PICKLE_PATH),"rb") as f:
+            WH = pickle.load(f)
+    except Exception as e:
+        print(e)
+        # What's with those weird symbols?
+        # u'\xbb' is our GO symbol (»)
+        # u'\xac' is our UNKNOWN symbol (¬)
+        # u'\xa4' is our END symbol (¤)
+        # They're arbitrarily chosen, but
+        # I think they both:
+        #   1). Are unlikely to appear in regular data, let alone cleaned data.
+        #   2). Look awesome.
+        vocab = [u'\xbb','|', '5', 'c', 'r', 'e', 'a', 't', 'u', '4', '6', 'h', 'm', 'n', ' ', 'o', 'd', 'l', 'i', '7', \
+                 '8', '&', '^', '/', '9', '{', 'W', '}', ',', 'T', ':', 's', 'y', 'b', 'f', 'v', 'p', '.', '3', \
+                 '0', 'A', '1', 'w', 'g', '\\', 'E', '@', '+', 'R', 'C', 'x', 'B', 'G', 'O', 'k', '"', 'N', 'U', \
+                 "'", 'q', 'z', '-', 'Y', 'X', '*', '%', '[', '=', ']', '~', 'j', 'Q', 'L', 'S', 'P', '2',u'\xac',u'\xf8',u'\xa4',u'\u00BB']
 
-        # Get dynamic batch_size
-        batch_size = tf.shape(x)[0]
-
-        #Inputs
-        #onehot = tf.reshape(tf.one_hot(x, N_CLASSES),[-1,N_CLASSES])
-        #rnn_inputs  = tf.reshape(tf.layers.dense(inputs=onehot, units=LSTM_SIZE,activation=tf.nn.sigmoid),[-1,1,LSTM_SIZE])
-        embedding = tf.get_variable("embedding", [N_CLASSES, LSTM_SIZE])
-        rnn_inputs = tf.nn.embedding_lookup(embedding, x)
-
-        #embedding = tf.get_variable("embedding", [N_CLASSES, LSTM_SIZE])
-        #rnn_inputs = tf.nn.embedding_lookup(embedding, x)
-
-        # RNN
-        lstm = tf.contrib.rnn.LSTMCell(LSTM_SIZE)
-        dropout = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=dropout_prob)
-        stacked_lstm = tf.contrib.rnn.MultiRNNCell([dropout] * NUM_LAYERS)
-
-        state = rnn_tuple_state
-        output_list = []
-        output, state = stacked_lstm(rnn_inputs[:,0], state)
-        output_list.append(tf.reshape(output,[-1,1,LSTM_SIZE]))
-        final_state = state
-        rnn_outputs = tf.concat(output_list,axis=1)
-
-        # rnn_outputs, final_state = tf.nn.dynamic_rnn(cell=stacked_lstm,
-        #                                              inputs=rnn_inputs,
-        #                                              initial_state=rnn_tuple_state)#stacked_lstm.zero_state(batch_size,tf.float32))
-        #
-
-        temp = tf.placeholder(tf.float32)
-
-        #Predictions, loss, training step
-        with tf.variable_scope("dense") as scope:
-            flat = tf.reshape(rnn_outputs, [-1, LSTM_SIZE])
-            dense = tf.layers.dense(inputs=flat, units=N_CLASSES)
-            logits = tf.reshape(dense,[-1,batch_size, N_CLASSES])
-        #pred = tf.nn.softmax(logits)
-        pred = tf.nn.softmax(tf.div(logits,temp))
-
-        losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
-        cost = tf.div(tf.reduce_sum(losses ), tf.cast(batch_size,tf.float32))
-
-        lr = tf.Variable(0.0, trainable=False)
-        tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), GRAD_CLIP)
-        with tf.name_scope('optimizer'):
-            op = tf.train.AdamOptimizer(lr)
-        optimizer = op.apply_gradients(zip(grads, tvars))
-
-        # Initialize variables
-        init = tf.global_variables_initializer()
-        # 'Saver' op to save and restore all the variables
-        saver = tf.train.Saver()
+        # Load mtg tokenized data
+        # Special thanks to mtgencode: https://github.com/billzorn/mtgencode
+        with open(data_path,"r") as f:
+             # Each card occupies its own line in this tokenized version
+             raw_txt = f.read()#.split("\n")
+        WH = word_helpers.WordHelper(raw_txt, vocab)
+        #WH = word_helpers.JSONHelper(data_path,vocab)
 
 
+        # Save our WordHelper
+        with open(os.path.join(dir_path,"data",PICKLE_PATH),"wb") as f:
+            pickle.dump(WH,f)
+    return WH
 
+# max_len = 75 # Determined by going through entire corpus
+# batch = WH.TrainBatches.next_card_batch(100,1)
+# for b in batch:
+#     print("".join([WH.vocab.vocab[i] for i in b]))
+# HODOR
+
+
+#with tf.device('/cpu:0'):
+#graph = tf.Graph()
+#with graph.as_default():
+
+# Placeholders
+x = tf.placeholder(tf.int32, [None, 1], name='input_placeholder')
+y = tf.placeholder(tf.int32, [None, 1], name='labels_placeholder')
+dropout_prob = tf.placeholder(tf.float32, name="dropout")
+
+# Our initial state placeholder:
+# NUM_LAYERS -- the number of layers used by our stacked LSTM
+# 2 -- 2 states for each layer (output,hidden)
+# None -- This will be our batch_size which is flexible
+# LSTM_SIZE -- the size of our hidden layers
+init_state = tf.placeholder(tf.float32, [NUM_LAYERS, 2, None, LSTM_SIZE], name='state_placeholder')
+# Create appropriate LSTMStateTuple for dynamic_rnn function out of our placeholder
+l = tf.unstack(init_state, axis=0)
+rnn_tuple_state = tuple(
+    [tf.contrib.rnn.LSTMStateTuple(l[idx][0], l[idx][1]) for idx in range(NUM_LAYERS)]
+)
+
+# Get dynamic batch_size
+batch_size = tf.shape(x)[0]
+
+#Inputs
+#onehot = tf.reshape(tf.one_hot(x, N_CLASSES),[-1,N_CLASSES])
+#rnn_inputs  = tf.reshape(tf.layers.dense(inputs=onehot, units=LSTM_SIZE,activation=tf.nn.sigmoid),[-1,1,LSTM_SIZE])
+embedding = tf.get_variable("embedding", [N_CLASSES, LSTM_SIZE])
+rnn_inputs = tf.nn.embedding_lookup(embedding, x)
+
+#embedding = tf.get_variable("embedding", [N_CLASSES, LSTM_SIZE])
+#rnn_inputs = tf.nn.embedding_lookup(embedding, x)
+
+# RNN
+lstm = tf.contrib.rnn.LSTMCell(LSTM_SIZE)
+dropout = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=dropout_prob)
+stacked_lstm = tf.contrib.rnn.MultiRNNCell([dropout] * NUM_LAYERS)
+
+state = rnn_tuple_state
+output_list = []
+output, state = stacked_lstm(rnn_inputs[:,0], state)
+output_list.append(tf.reshape(output,[-1,1,LSTM_SIZE]))
+final_state = tf.identity(state, name="final_state")
+
+rnn_outputs = tf.concat(output_list,axis=1)
+
+# rnn_outputs, final_state = tf.nn.dynamic_rnn(cell=stacked_lstm,
+#                                              inputs=rnn_inputs,
+#                                              initial_state=rnn_tuple_state)#stacked_lstm.zero_state(batch_size,tf.float32))
+#
+
+temp = tf.placeholder(tf.float32, name="temp")
+
+#Predictions, loss, training step
+with tf.variable_scope("dense") as scope:
+    flat = tf.reshape(rnn_outputs, [-1, LSTM_SIZE])
+    dense = tf.layers.dense(inputs=flat, units=N_CLASSES)
+    logits = tf.reshape(dense,[-1,batch_size, N_CLASSES])
+#pred = tf.nn.softmax(logits)
+pred = tf.nn.softmax(tf.div(logits,temp),name="pred")
+
+losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+cost = tf.div(tf.reduce_sum(losses ), tf.cast(batch_size,tf.float32))
+
+lr = tf.Variable(0.0, name="learning_rate", trainable=False)
+tvars = tf.trainable_variables()
+grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), GRAD_CLIP)
+with tf.name_scope('optimizer'):
+    op = tf.train.AdamOptimizer(lr)
+optimizer = op.apply_gradients(zip(grads, tvars))
+
+# Initialize variables
+#init = tf.global_variables_initializer()
+# 'Saver' op to save and restore all the variables
+saver = tf.train.Saver()
+
+
+
+if __name__ == "__main__":
     print("Beginning Session")
     #  TRAINING Parameters
-
+    WH = getWordHelpers()
     #Running first session
-    with tf.Session(graph=graph) as sess:
+    with tf.Session() as sess:
         # Initialize variables
         #sess.run(init)
         sess.run(tf.global_variables_initializer())
@@ -203,7 +209,7 @@ with tf.device('/cpu:0'):
         # Training cycle
         for epoch in range(already_trained,already_trained+NUM_EPOCHS):
             # Set learning rate
-            sess.run(tf.assign(lr,LEARNING_RATE * (DECAY_RATE ** (epoch % DECAY_STEP)   )))
+            sess.run(tf.assign(lr,LEARNING_RATE * (DECAY_RATE ** (epoch % DECAY_STEP))  ))
 
             start = time.time()
             sum_cost = 0
@@ -273,6 +279,7 @@ with tf.device('/cpu:0'):
                         #    true.append(WH.vocab.id2char(batch_y[0][l]))
                     print(" ") # Spacer
                     print("PRED: {}".format(''.join(preds)))
+                    save_path = saver.save(sess, model_path, global_step = epoch)
                     #print("TRUE: {}".format(''.join(true)))
 
             end = time.time()
---
+++ shakespeare_sample.py
@@ -0,0 +1,112 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+import os
+import numpy as np
+import random
+import tensorflow as tf
+import os
+import word_helpers
+import pickle
+import time
+#import caffeine
+
+from shakespeare import getWordHelpers,weighted_pick, SAVE_DIR,CHECKPOINT_NAME,DATA_NAME,PICKLE_PATH,SUBDIR_NAME
+from shakespeare import NUM_LAYERS, LSTM_SIZE
+print("Beginning Session")
+#  TRAINING Parameters
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(dir_path,"saved",SAVE_DIR,CHECKPOINT_NAME)
+checkpoint_path = os.path.join(dir_path,"saved",SAVE_DIR)
+data_path = os.path.join(dir_path,"data",SUBDIR_NAME,DATA_NAME)
+PRIME_TEXT = "[SCENE I]"
+TEMPERATURE = 0.5
+NUM_PRED = 100
+
+WH = getWordHelpers()
+
+saver = tf.train.import_meta_graph(os.path.join(checkpoint_path,"shakespeare_rec_char_steps.ckpt-0.meta"))
+
+# We can now access the default graph where all our metadata has been loaded
+graph = tf.get_default_graph()
+
+lr = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="learning_rate")[0]
+
+final_state = graph.get_tensor_by_name('final_state:0')
+pred = graph.get_tensor_by_name('pred:0')
+x = graph.get_tensor_by_name('input_placeholder:0')
+y = graph.get_tensor_by_name('labels_placeholder:0')
+init_state = graph.get_tensor_by_name('state_placeholder:0')
+dropout_prob = graph.get_tensor_by_name('dropout:0')
+temp = graph.get_tensor_by_name('temp:0')
+
+#Running first session
+with tf.Session(graph=graph) as sess:
+    # Initialize variables
+    #sess.run(init)
+    sess.run(tf.global_variables_initializer())
+
+    try:
+        ckpt = tf.train.get_checkpoint_state(checkpoint_path)
+        # A quirk of training on a different machine:
+        # the model_checkpoint_path is an absolute path and
+        # makes restoring fail because it doesn't match the path here.
+        # To avoid this, we extract the checkpoint file name
+        # then recreate the correct path and restore from there.
+        restore_path = ckpt.model_checkpoint_path
+        restore_file = os.path.basename(restore_path)
+        ckpt_file = os.path.basename(restore_path)
+        already_trained = int(ckpt_file.replace(CHECKPOINT_NAME+"-",""))
+        new_path = os.path.join(dir_path,"saved",SAVE_DIR,restore_file)
+        saver.restore(sess, new_path)#ckpt.model_checkpoint_path)
+        print("Model restored from file: %s" % model_path)
+        print("__________________________________________")
+    except Exception as e:
+        print("Model restore failed {}".format(e))
+        print("__________________________________________")
+
+
+    # Set learning rate
+    sess.run(tf.assign(lr,0))
+
+    # Test model
+    preds = []
+    true = []
+
+    # We no longer use BATCH_SIZE here because
+    # in the test method we only want to compare
+    # one card output to one card prediction
+    init_x = np.array([WH.TrainBatches.vocab.go]).reshape((1,1))
+    preds = [c for c in PRIME_TEXT] + [" "]
+    unused_y = np.zeros((1,1))
+    state = np.zeros((NUM_LAYERS,2,1,LSTM_SIZE))
+
+    # Begin our primed text Feeding
+    for c in PRIME_TEXT:
+        prime_x = np.array([WH.vocab.vocab.index(c)]).reshape((1,1))
+        s, = sess.run([final_state], feed_dict={x: prime_x,
+                                                   y: unused_y,
+                                                   init_state: state,
+                                                   dropout_prob: 1.0,
+                                                   temp:TEMPERATURE})
+        state = s
+
+    # We iterate over every pair of letters in our test batch
+    init_x = np.array([WH.TrainBatches.vocab.vocab.index(' ')]).reshape((1,1))
+    for i in range(0,NUM_PRED):
+        s,p = sess.run([final_state, pred], feed_dict={x: init_x,
+                                                       y: unused_y,
+                                                       init_state: state,
+                                                       dropout_prob: 1.0,
+                                                       temp:TEMPERATURE})
+        state = s
+        # Choose a letter from our vocabulary based on our output probability: p
+        for j in p:
+            #pred_letter = np.random.choice(WH.vocab.vocab, 1, p=j[0])[0]
+            pred_index = weighted_pick(j)
+            pred_letter = WH.vocab.vocab[pred_index]
+            preds.append(pred_letter)
+            init_x = np.array([[pred_index]])
+    print(" ") # Spacer
+    print("PRED: {}".format(''.join(preds)))
+    #print("TRUE: {}".format(''.join(true)))
